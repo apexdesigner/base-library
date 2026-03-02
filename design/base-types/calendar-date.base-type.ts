73a492deb71b96ce067ed4746d11e4b560ab2218
David Knapp <david.knapp@apexbpm.com>
2026-03-02 16:45:23 -0600

    feat: move Email and Json base types from testing to real base-types
    - Move Email from testing/base-types with presentAs: "email" (removed VARCHAR column)
    - Move Json from testing/base-types with presentAs: "json" and jsonb column
    - Delete duplicate Uuid from testing (already exists in base-types)
    - Add column type: "date" to CalendarDate base type

--- design/base-types/calendar-date.base-type.ts
+++ design/base-types/calendar-date.base-type.ts
@@ -2,4 +2,4 @@ import { BaseType, setPropertyDefaults } from "@apexdesigner/dsl";
 
 export class CalendarDate extends BaseType<string> {}
 
-setPropertyDefaults(CalendarDate, { presentAs: "date" });
+setPropertyDefaults(CalendarDate, { presentAs: "date", column: { type: "date" } });
--- design/testing/base-types/uuid.base-type.ts
+++
@@ -1,5 +0,0 @@
-import { BaseType, setPropertyDefaults } from "@apexdesigner/dsl";
-
-export class Uuid extends BaseType<string> {}
-
-setPropertyDefaults(Uuid, { column: "UUID" });
