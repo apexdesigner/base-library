d7f3dfc3c8f0ced47d5aed72c04e6c80be1338e2
David Knapp <david.knapp@apexbpm.com>
2026-03-10 11:09:16 -0500

    chore: bump @apexdesigner dependency selectors to latest versions

--- design/project.ts
+++ design/project.ts
@@ -10,7 +10,7 @@ export class BaseLibrary extends Project {
 
   isLibrary = true;
 
-  designDependencies = [{ package: '@apexdesigner/doc-generators', versionSelector: '^0.0.10' }];
+  designDependencies = [{ package: '@apexdesigner/doc-generators', versionSelector: '^0.0.11' }];
 
   parameterValues = {
     formFieldAppearance: 'outline',
@@ -58,7 +58,7 @@ export class BaseLibrary extends Project {
   };
 
   serverDependencies = {
-    '@apexdesigner/schema-persistence': '^0.1.87',
+    '@apexdesigner/schema-persistence': '^0.1.88',
     '@apexdesigner/schema-tools': '^0.1.76',
     debug: '^4.4.3',
     express: '^5.1.0',
