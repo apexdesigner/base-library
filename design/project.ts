da777cf032d91db9c662bc8b507e966cb5963cb0
Dave Knapp <david.knapp@apexbpm.com>
2026-03-19 15:08:52 -0500

    chore: update dependencies and format external-items
    Bump @apexdesigner/dsl to ^0.8.69, @apexdesigner/server to ^0.8.43,
    and @apexdesigner/doc-generators to ^0.1.5. Format external-items
    design files with consistent quote style and added descriptions.

--- design/project.ts
+++ design/project.ts
@@ -10,7 +10,7 @@ export class BaseLibrary extends Project {
 
   isLibrary = true;
 
-  designDependencies = [{ package: '@apexdesigner/doc-generators', versionSelector: '^0.1.2' }];
+  designDependencies = [{ package: '@apexdesigner/doc-generators', versionSelector: '^0.1.5' }];
 
   parameterValues = {
     formFieldAppearance: 'outline',
