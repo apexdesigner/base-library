5043e460c6939230c933931f8cc167e2febc9d60
Dave Knapp <david.knapp@apexbpm.com>
2026-03-26 04:41:00 -0500

    feat: add export-tsv-button component (#3337)
    Icon button that exports a business object array to a TSV file download.
    Auto-derives columns from entity metadata. Added to test-items page.

--- design/testing/pages/test-items.page.ts
+++ design/testing/pages/test-items.page.ts
@@ -1,6 +1,7 @@
 import { Page, page, property, applyTemplate } from '@apexdesigner/dsl/page';
 import { TestItemPersistedArray } from '@business-objects-client';
 import { AddButtonComponent } from '@components';
+import { ExportTsvButtonComponent } from '@components';
 import { RefreshButtonComponent } from '@components';
 
 /**
@@ -31,6 +32,10 @@ applyTemplate(TestItemsPage, [
         contains: [
           { h1: 'Test Items' },
           { element: 'div', attributes: { grow: null } },
+          {
+            element: 'export-tsv-button',
+            attributes: { array: '<- testItems' },
+          },
           {
             element: 'refresh-button',
             attributes: { array: '<- testItems' },
